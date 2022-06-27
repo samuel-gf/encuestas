b507504628348bd4ff1ddc36409c02d52a87e4bf
Samuel Gómez <profesor_s@outlook.com>
2022-06-27 09:41:24 +0200

    27 junio 2022

--- _src/index.php
+++ _src/index.php
@@ -11,7 +11,7 @@ include("lib.php");
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <meta name="description" content="Encuesta alumnado">
 <link rel="stylesheet" type="text/css" href="css/normalize.css">
-<link rel="stylesheet" type="text/css" href="css/votacion.css">
+<link rel="stylesheet" type="text/css" href="css/encuesta.css">
 <script type="text/javascript" src="js/encuesta.js"></script>
 <title>Encuesta alumnado</title>
 </head>
